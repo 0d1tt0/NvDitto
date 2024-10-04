5a7f2b6c179789b9d731034e39e20186b66165e2
0d1tt0 <d1tt0@ferax.cx>
2024-10-04 13:19:16 -0500

    init

---
+++ lua/configs/lazy.lua
@@ -0,0 +1,8 @@
+return {
+    defaults = {lazy = true},
+    install = {colorscheme = {"nvchad"}},
+
+    ui = {
+        icons = {ft = "", lazy = "󰂠 ", loaded = "", not_loaded = ""}
+    }
+}
---
+++ lua/configs/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+    ensure_installed = { "lua", "c", "cpp", "cmake", "make", "python", "rust", "toml", "javascript", "markdown" },
+  
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+  
+    indent = { enable = true },
+}
